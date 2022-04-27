40f3c89ca9937763d68c13716e118b7486041880
Marcus <marcusmoraes2010@hotmail.com>
2022-04-27 10:16:32 -0300

    Preparando revisão

--- src/controllers/virtualCardsController.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-
-export async function createNewCardVirtual(req: Request, res: Response) {
-  res.sendStatus(201);
-}
-
-export async function deleteCardVirtual(req: Request, res: Response) {
-  res.sendStatus(200);
-}
